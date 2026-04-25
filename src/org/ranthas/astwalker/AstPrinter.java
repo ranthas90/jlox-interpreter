ece0b54ec9929b539c954dc7d47b8e6393b66364
ranthas90 <rubenreal1990@gmail.com>
2026-04-25 10:58:15 +0100

    Initial commit

---
+++ src/org/ranthas/astwalker/AstPrinter.java
@@ -0,0 +1,60 @@
+package org.ranthas.astwalker;
+
+class AstPrinter implements Expr.Visitor<String> {
+
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitLogicalExpr(Expr.Logical expr) {
+        return "Not implemented yet"; // TODO: pendiente de implementar;
+    }
+
+    @Override
+    public String visitAssignExpr(Expr.Assign expr) {
+        return "Not implemented yet"; // TODO: pendiente de implementar
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return parenthesize("group", expr.expression);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null) {
+            return "nil";
+        }
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return parenthesize(expr.operator.lexeme, expr.right);
+    }
+
+    @Override
+    public String visitVariableExpr(Expr.Variable expr) {
+        return "Not implemented yet!"; // TODO: pendiente de implementar!!!
+    }
+
+    private String parenthesize(String name, Expr... expressions) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("(").append(name);
+
+        for (Expr expr : expressions) {
+            stringBuilder.append(" ");
+            stringBuilder.append(expr.accept(this));
+        }
+        stringBuilder.append(")");
+
+        return stringBuilder.toString();
+    }
+}
---
+++ src/org/ranthas/astwalker/Expr.java
@@ -0,0 +1,125 @@
+package org.ranthas.astwalker;
+
+abstract class Expr {
+    interface Visitor<T> {
+        T visitAssignExpr(Assign expr);
+        T visitBinaryExpr(Binary expr);
+        T visitGroupingExpr(Grouping expr);
+        T visitLiteralExpr(Literal expr);
+        T visitLogicalExpr(Logical expr);
+        T visitUnaryExpr(Unary expr);
+        T visitVariableExpr(Variable expr);
+    }
+
+    abstract <T> T accept(Visitor<T> visitor);
+
+    static class Assign extends Expr {
+
+        final Token name;
+        final Expr value;
+
+        Assign(Token name, Expr value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        @Override
+        <T> T accept(Visitor<T> visitor) {
+            return visitor.visitAssignExpr(this);
+        }
+    }
+
+    static class Binary extends Expr {
+
+        final Expr left;
+        final Token operator;
+        final Expr right;
+
+        Binary(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+
+        @Override
+        <T> T accept(Visitor<T> visitor) {
+            return visitor.visitBinaryExpr(this);
+        }
+    }
+
+    static class Grouping extends Expr {
+
+        final Expr expression;
+
+        Grouping(Expr expression) {
+            this.expression = expression;
+        }
+
+        @Override
+        <T> T accept(Visitor<T> visitor) {
+            return visitor.visitGroupingExpr(this);
+        }
+    }
+
+    static class Literal extends Expr {
+
+        final Object value;
+
+        Literal(Object value) {
+            this.value = value;
+        }
+
+        @Override
+        <T> T accept(Visitor<T> visitor) {
+            return visitor.visitLiteralExpr(this);
+        }
+    }
+
+    static class Logical extends Expr {
+
+        final Expr left;
+        final Token operator;
+        final Expr right;
+
+        Logical(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+
+        @Override
+        <T> T accept(Visitor<T> visitor) {
+            return visitor.visitLogicalExpr(this);
+        }
+    }
+
+    static class Unary extends Expr {
+
+        final Token operator;
+        final Expr right;
+
+        Unary(Token operator, Expr right) {
+            this.operator = operator;
+            this.right = right;
+        }
+
+        @Override
+        <T> T accept(Visitor<T> visitor) {
+            return visitor.visitUnaryExpr(this);
+        }
+    }
+
+    static class Variable extends Expr {
+
+        final Token name;
+
+        Variable(Token name) {
+            this.name = name;
+        }
+
+        @Override
+        <T> T accept(Visitor<T> visitor) {
+            return visitor.visitVariableExpr(this);
+        }
+    }
+}
