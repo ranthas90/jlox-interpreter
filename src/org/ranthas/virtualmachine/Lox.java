e5c3528fd3ecd9b751ae23e2729a977d5b7e55a3
AdQ-RubenReal <ruben.real@adquiver.com>
2026-05-14 15:35:10 +0100

    wip - virtual machine

---
+++ src/org/ranthas/virtualmachine/Lox.java
@@ -0,0 +1,73 @@
+package org.ranthas.virtualmachine;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Lox {
+
+    private static VirtualMachine virtualMachine = new VirtualMachine();
+
+    public static void main(String[] args) throws IOException {
+        /*
+        Chunk chunk = new Chunk();
+
+        int constantIndex = chunk.writeConstant(1.2);
+        chunk.writeCode(OpCode.CONSTANT, 123);
+        chunk.writeCode((byte)constantIndex, 123);
+
+        constantIndex = chunk.writeConstant(3.4);
+        chunk.writeCode(OpCode.CONSTANT, 123);
+        chunk.writeCode((byte)constantIndex, 123);
+
+        chunk.writeCode(OpCode.ADD, 123);
+
+        constantIndex = chunk.writeConstant(5.6);
+        chunk.writeCode(OpCode.CONSTANT, 123);
+        chunk.writeCode((byte)constantIndex, 123);
+
+        chunk.writeCode(OpCode.DIVIDE, 123);
+        chunk.writeCode(OpCode.NEGATE, 123);
+
+        chunk.writeCode(OpCode.RETURN, 123);
+
+        //Debugger debugger = new Debugger();
+        //debugger.disassembleChunk(chunk, "test chunk");
+        VirtualMachine virtualMachine = new VirtualMachine();
+        virtualMachine.interpret(chunk);
+         */
+        if (args.length > 1) {
+            System.err.println("Usage: jlox [path]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            //repl();
+            runFile("./test/assignment/associativity.lox");
+            //runFile("./test/block/empty.lox");
+            //runFile("./test/field/get_on_bool.lox");
+        }
+    }
+
+    private static void repl() throws IOException {
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        for (;;) {
+            System.out.print("> ");
+            String line = bufferedReader.readLine();
+            if (line == null) {
+                break;
+            }
+            virtualMachine.interpret(line);
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        virtualMachine.interpret(new String(bytes, Charset.defaultCharset()));
+    }
+}
---
+++ src/org/ranthas/virtualmachine/OpCode.java
@@ -0,0 +1,11 @@
+package org.ranthas.virtualmachine;
+
+public class OpCode {
+    public static final byte CONSTANT =     0x00;
+    public static final byte ADD =          0x01;
+    public static final byte SUBTRACT =     0x02;
+    public static final byte MULTIPLY =     0x03;
+    public static final byte DIVIDE =       0x04;
+    public static final byte NEGATE =       0x05;
+    public static final byte RETURN =       0x06;
+}
